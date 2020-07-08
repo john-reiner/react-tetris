a2cdb847f1aab95178c2f8897471e92c09666102
jareiner90 <johnreiner1990@gmail.com>
2020-07-08 14:49:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+import Tetris from './components/Tetris';
+
+
+
+
+const App = () =>  {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/users')
+    .then(response => response.json())
+    .then(users => setUsers(users))
+  }, [])
+  
+  
+
+  return (
+    <div className="App">
+      <Tetris  />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LogIn.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+
+export default function LogIn() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [show, setShow] = useState(true);
+
+    const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
+    // state = {
+    //     userName: '',
+    //     passWord: '',
+    // }
+
+    const handleUsernameChange = e => setUsername(e.target.value)
+    const handlePasswordChange = e => setPassword(e.target.value)
+
+    const handleChange = e => {
+        // setState({
+        //     [e.target.name]: e.target.value
+        // })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+    
+
+    return (
+        <div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+{/* <form>
+<input name="userName" placeholder='User Name' value={username} onChange={handleChange}/>
+<br />
+<input name="passWord" type="password" placeholder='Password' value={password} onChange={handleChange}/>
+<br />
+<button onClick={onSubmit}>Submit</button>
+<Link to='/sign-up'><button>Sign Up</button></Link>
+</form> */}
